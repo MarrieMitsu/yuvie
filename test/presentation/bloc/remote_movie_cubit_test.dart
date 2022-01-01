314e0abadf026ac2fb51cc4d1ba666782006fc91
Isnainromadhoni <isnainromadoni@gmail.com>
2022-01-01 13:47:48 +0700

    test: Add test for presentation layer
    Signed-off-by: Isnainromadhoni <isnainromadoni@gmail.com>

---
+++ test/presentation/bloc/remote_movie_cubit_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:yuvie/domain/usecases/movie/movie.dart';
+import 'package:yuvie/presentation/blocs/movie/remote_movie_cubit.dart';
+
+import 'remote_movie_cubit_test.mocks.dart';
+
+@GenerateMocks([SearchRemoteMovies, GetDetailRemoteMovie])
+void main() {
+  late MockSearchRemoteMovies searchRemoteMovies;
+  late MockGetDetailRemoteMovie getDetailRemoteMovie;
+
+  late RemoteMovieCubit remoteMovieCubit;
+
+  setUp(() {
+    searchRemoteMovies = MockSearchRemoteMovies();
+    getDetailRemoteMovie = MockGetDetailRemoteMovie();
+
+    remoteMovieCubit = RemoteMovieCubit(
+      searchRemoteMovies: searchRemoteMovies, 
+      getDetailRemoteMovie: getDetailRemoteMovie
+    );
+  });
+
+  test('state should be initial', () {
+    expect(remoteMovieCubit.state, equals(RemoteMovieInitial()));
+  });
+
+}
