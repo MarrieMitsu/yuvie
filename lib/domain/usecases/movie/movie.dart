43e3b406e34a834b8033d6353380b88eb00fffaf
Isnainromadhoni <isnainromadoni@gmail.com>
2022-01-02 10:41:05 +0700

    domain: Register usecases barrel files
    Signed-off-by: Isnainromadhoni <isnainromadoni@gmail.com>

--- lib/domain/usecases/movie/movie.dart
+++ lib/domain/usecases/movie/movie.dart
@@ -1,5 +1,6 @@
 export 'search_remote_movies.dart';
 export 'get_detail_remote_movie.dart';
+export 'get_all_local_genre.dart';
 export 'get_all_local_movie.dart';
 export 'get_detail_local_movie.dart';
 export 'add_local_movie.dart';
