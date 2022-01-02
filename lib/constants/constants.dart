901d34a09b6fcae7ba0e76c4ee2c002dc50bf6f9
Isnainromadhoni <isnainromadoni@gmail.com>
2022-01-02 10:37:50 +0700

    constants: Add Database and API enum type request
    Signed-off-by: Isnainromadhoni <isnainromadoni@gmail.com>

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -7,3 +7,25 @@ enum MovieSource {
   LOCAL,
   REMOTE
 }
+
+enum APIOperation {
+  None,
+  GET,
+  POST,
+}
+
+enum DBOperation {
+  None,
+  Read,
+  Create,
+  Update,
+  Delete,
+}
+
+class PosterUrl {
+  
+  static String w92(String posterPath) => 'http://image.tmdb.org/t/p/w92$posterPath';
+  static String w185(String posterPath) => 'http://image.tmdb.org/t/p/w185$posterPath';
+  static String w500(String posterPath) => 'http://image.tmdb.org/t/p/w500$posterPath';
+
+}
